50142b959fdab5f003c807978c5c6941c2395670
Rmdru <rmdruijter@gmail.com>
2024-04-20 16:47:24 +0200

    [VM-3] Vehicle table added

--- app/Filament/Resources/VehicleResource.php
+++ app/Filament/Resources/VehicleResource.php
@@ -12,6 +12,7 @@ use Filament\Tables\Table;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\DatePicker;
+use Filament\Tables\Columns\TextColumn;
 
 class VehicleResource extends Resource
 {
@@ -43,7 +44,7 @@ class VehicleResource extends Resource
                     ->numeric()
                     ->inputMode('decimal'),
                 TextInput::make('mileage_start')
-                    ->label(__('Mileage start'))
+                    ->label(__('Mileage begin'))
                     ->numeric(),
                 DatePicker::make('purchase_date')
                     ->label(__('Purchase date'))
@@ -55,7 +56,7 @@ class VehicleResource extends Resource
                     ->required()
                     ->prefix('NL')
                     ->extraInputAttributes(['class' => '!text-black bg-yellow-600']),
-                Select::make('Fuel type')
+                Select::make('fuel_type')
                     ->label(__('Fuel type'))
                     ->options(config('cars.fuel_types')),
                 Toggle::make('private')
@@ -65,9 +66,35 @@ class VehicleResource extends Resource
 
     public static function table(Table $table): Table
     {
+        $brands = config('cars.brands');
+        $fuelTypes = config('cars.fuel_types');
+
         return $table
             ->columns([
-                //
+                TextColumn::make('brand')
+                    ->label(__('Brand'))
+                    ->formatStateUsing(fn (string $state) => $brands[$state] ?? $state),
+                TextColumn::make('model')
+                    ->label(__('Model')),
+                TextColumn::make('version')
+                    ->label(__('Version')),
+                TextColumn::make('engine')
+                    ->label(__('Engine')),
+                TextColumn::make('factory_specification_fuel_consumption')
+                    ->label(__('Factory specification for fuel consumption')),
+                TextColumn::make('mileage_start')
+                    ->label(__('Mileage begin')),
+                TextColumn::make('mileage_latest')
+                    ->label(__('Latest mileage')),
+                TextColumn::make('purchase_date')
+                    ->label(__('Purchase date')),
+                TextColumn::make('license_plate')
+                    ->label(__('License plate')),
+                TextColumn::make('fuel_type')
+                    ->label(__('Fuel type'))
+                    ->formatStateUsing(fn (string $state) => $fuelTypes[$state] ?? $state),
+                TextColumn::make('private')
+                    ->label(__('Private')),
             ])
             ->filters([
                 //
