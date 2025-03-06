3d8454be85ab491fb10900131acbcc46173da5c6
Ruben de Ruijter <rmdruijter@gmail.com>
2025-03-06 20:06:10 +0100

    [VM-54] Implement P+R option

--- app/Enums/ParkingType.php
+++ app/Enums/ParkingType.php
@@ -11,12 +11,16 @@ enum ParkingType: string implements HasLabel, HasIcon
 {
     case Street = 'street';
     case Garage = 'garage';
+    case ParkAndRide = 'park_and_ride';
+    case Company = 'company';
 
     public function getLabel(): string
     {
         return match ($this) {
             self::Street => __('Street'),
             self::Garage => __('Parking garage'),
+            self::ParkAndRide => __('P+R'),
+            self::Company => __('Company'),
         };
     }
 
@@ -25,6 +29,8 @@ enum ParkingType: string implements HasLabel, HasIcon
         return match ($this) {
             self::Street => 'maki-parking-paid',
             self::Garage => 'maki-parking-garage',
+            self::ParkAndRide => 'gmdi-train',
+            self::Company => 'mdi-office-building',
         };
     }
 }
