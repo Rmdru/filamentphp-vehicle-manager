762512aeca938ab6bb671074fe92e4e35f93a5da
Ruben de Ruijter <rmdruijter@gmail.com>
2025-03-07 20:09:03 +0100

    [VM-128] Phase out livewire auth

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,8 @@
 <?php
 
-use App\Http\Controllers\Auth\EmailVerificationController;
-use App\Http\Controllers\Auth\LogoutController;
 use App\Http\Controllers\MaintenanceController;
-use App\Livewire\Auth\Login;
-use App\Livewire\Auth\Passwords\Confirm;
-use App\Livewire\Auth\Passwords\Email;
-use App\Livewire\Auth\Passwords\Reset;
-use App\Livewire\Auth\Register;
-use App\Livewire\Auth\Verify;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\VehicleController;
-use App\Models\Maintenance;
 
 /*
 |--------------------------------------------------------------------------
@@ -24,39 +15,13 @@ use App\Models\Maintenance;
 |
 */
 
-Route::view('/', 'welcome')->name('home');
-
 Route::middleware('guest')->group(function () {
-    Route::get('login', Login::class)
-        ->name('login');
-
-    Route::get('register', Register::class)
-        ->name('register');
-});
-
-Route::get('password/reset', Email::class)
-    ->name('password.request');
-
-Route::get('password/reset/{token}', Reset::class)
-    ->name('password.reset');
-
-Route::middleware('auth')->group(function () {
-    Route::get('email/verify', Verify::class)
-        ->middleware('throttle:6,1')
-        ->name('verification.notice');
-
-    Route::get('password/confirm', Confirm::class)
-        ->name('password.confirm');
+    Route::get('/', function () {
+        return redirect(route('filament.account.pages.dashboard'));
+    });
 });
 
 Route::middleware('auth')->group(function () {
-    Route::get('email/verify/{id}/{hash}', EmailVerificationController::class)
-        ->middleware('signed')
-        ->name('verification.verify');
-
-    Route::post('logout', LogoutController::class)
-        ->name('logout');
-
     Route::get('/account/switch-vehicle/{vehicleId}', [VehicleController::class, 'switchVehicle'])
         ->name('switch-vehicle');
 
